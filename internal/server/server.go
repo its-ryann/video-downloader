dfe4905c72ccf3def7bc26b29a7511870d2b4e42
its-ryann <kikayiryankim@gmail.com>
2026-04-01 01:48:25 +0300

    refactor(server): replace synchronous handler with async job endpoints

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,18 +3,18 @@ package server
 import (
 	"fmt"
 	"net/http"
-
 	"video-downloader/internal/api"
 )
 
 func Start() {
-	// Creates a semaphore with 3 slots - max of 3 downloads at once.
 	sem := make(chan struct{}, 3)
 
 	fs := http.FileServer(http.Dir("./web"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/download", api.DownloadHandler(sem))
+	http.HandleFunc("/download", api.StartDownload(sem))
+	http.HandleFunc("/progress/", api.GetProgress)
+	http.HandleFunc("/file/", api.ServeFile)
 
 	fmt.Println("Server running on :8080")
 	http.ListenAndServe(":8080", nil)
